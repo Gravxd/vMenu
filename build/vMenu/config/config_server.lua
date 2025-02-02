fb2ae947fbb3a7485e63d59cbc357dd69bfaea2c
Grav <75702884+Gravxd@users.noreply.github.com>
2025-02-02 00:52:55 +0000

    tweak(config/server) remove sql resource, add disable ai

--- build/vMenu/config/config_server.lua
+++ build/vMenu/config/config_server.lua
@@ -1,4 +1,4 @@
 Config = {
-    SQLResource = "oxmysql",
     EnableServerList = true, -- If you want to show the framework version in your server list info convars.
+    DisableAI = GetConvarBool("vmenu_disable_ai", false), -- This will disable NPC spawning in bucket 0 (if you don't touch buckets, you don't need to worry about this). This is useful for people who want a quick and easy way to disable AI.
 }
